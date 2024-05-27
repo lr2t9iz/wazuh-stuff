1c4048f30c627861c2d85af5d11610db0c78946a
lr2t9iz <mitzepx01@gmail.com>
2024-05-27 07:17:44 -0600

    chore: new rules

---
+++ update_windows_rules.sh
@@ -0,0 +1,13 @@
+D1R="$(dirname $0)"
+WzDIR="/var/ossec"
+rule_files=("aaa_w1n_overwrite.xml", "abc_l1n_rul3s.xml")
+
+# Update changes
+for r_file in ${rule_files[@]}; do
+  cp "${D1R}/windows/detection-rules/$r_file" "${WzDIR}/etc/rules/$r_file"
+  chmod -R 660 "${WzDIR}/etc/rules/$r_file"
+  chown -R  wazuh:wazuh "${WzDIR}/etc/rules/$r_file"
+done
+
+# Apply changes
+"${WzDIR}/bin/wazuh-control" restart
