dd96f3c64d205aea469488edc1867652f4ba147a
lr2t9iz <mitzepx01@gmail.com>
2024-05-31 06:24:44 -0600

    chore: windows source

---
+++ bin/update_windows_sources.sh
@@ -0,0 +1,4 @@
+D1R="$(dirname $0)/.."
+WzDIR="/var/ossec"
+
+cp "${D1R}/windows/w1ndows_s0urces.xml" "${WzDIR}/etc/shared/w1ndows_s0urces/agent.conf"
--- update_windows_sources.sh
+++
@@ -1,3 +0,0 @@
-D1R="$(dirname $0)"
-WzDIR="/var/ossec"
-cp "${D1R}/windows/w1ndows_s0urces.conf" "${WzDIR}/etc/shared/w1ndows_s0urces/agent.conf"
