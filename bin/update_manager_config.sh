75c5983da8caf1f5da887b6fa9130d356c5c1669
lr2t9iz <mitzepx01@gmail.com>
2024-05-31 17:50:23 -0600

    chore: manager config

--- bin/update_manager_config.sh
+++ bin/update_manager_config.sh
@@ -2,3 +2,10 @@ D1R="$(dirname $0)/.."
 WzDIR="/var/ossec"
 
 cp "${D1R}/resources/manager/default_group.xml" "${WzDIR}/etc/shared/default/agent.conf"
+
+# edit manager config <- creds
+sed -i "s|SLACK_HOOK|${SLACK_HOOK}|g" "${D1R}/resources/manager/manager.xml"
+# sed -i "s|TEAMS_HOOK|${TEAMS_HOOK}|g" "${D1R}/resources/manager/manager.xml"
+
+cp "${D1R}/resources/manager/manager.xml"  "${WzDIR}/etc/ossec.conf"
+"${WzDIR}/bin/wazuh-control" restart
